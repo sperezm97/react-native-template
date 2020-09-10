5476de92d172ac44ed4be08a13093c934ded59cf
Sebastian Perez <sperezm97@gmail.com>
2020-09-09 21:46:50 -0400

    fix linters errores

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native-community|@react-navigation)',
+  ],
 };
